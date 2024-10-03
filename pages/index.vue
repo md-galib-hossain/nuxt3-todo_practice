a24ee0c800b65164ee762b6ed8a01f7804878c73
Md Galib Hossain <mdgalib23@gmail.com>
2024-10-03 18:12:22 +0600

    v1 launch

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
-<template>
+ <template>
   <div class="max-w-screen-lg mx-auto">
     <div class="bg-gray-50 p-6 rounded-lg shadow-md mt-8">
       <h1 class="text-3xl font-bold text-gray-800 mb-2">Todo List</h1>
-      
+
       <p v-if="errorMessages.length" class="text-red-500 text-sm mb-4">
-        {{ errorMessages.join(', ') }}
+        {{ errorMessages.join(", ") }}
       </p>
 
       <div class="flex items-center gap-4 mb-4">
@@ -22,8 +22,8 @@
           placeholder="Enter todo description"
           class="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
         />
-        <button 
-          @click="addTodo" 
+        <button
+          @click="addTodo"
           class="bg-orange-500 text-white px-4 py-2 rounded-lg shadow hover:bg-orange-600 transition duration-200"
         >
           Add Todo
@@ -37,21 +37,30 @@
           class="flex justify-between items-center bg-orange-50 rounded-lg p-4 shadow"
         >
           <div>
-            <h3 :class="[{ 'line-through': item.completed }, 'text-lg font-semibold text-gray-800']">{{ item.title }}</h3>
+            <h3
+              :class="[
+                { 'line-through': item.completed },
+                'text-lg font-semibold text-gray-800',
+              ]"
+            >
+              {{ item.title }}
+            </h3>
             <p class="text-gray-600">{{ item.description }}</p>
           </div>
           <div class="flex space-x-2">
-            <button 
-              @click="markAsComplete(index)" 
-              :class="[ 
-                item.completed ? 'bg-orange-500 hover:bg-orange-600' : 'bg-green-500 hover:bg-green-400', 
-                'text-white px-3 py-1 rounded transition duration-200'
+            <button
+              @click="markAsComplete(item._id, index)"
+              :class="[
+                item.completed
+                  ? 'bg-orange-500 hover:bg-orange-600'
+                  : 'bg-green-500 hover:bg-green-400',
+                'text-white px-3 py-1 rounded transition duration-200',
               ]"
             >
               {{ item.completed ? "Completed" : "Complete" }}
             </button>
-            <button 
-              @click="removeFromTodo(index)" 
+            <button
+              @click="removeFromTodo(item._id,index)"
               class="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-400 transition duration-200"
             >
               x
@@ -65,49 +74,77 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 
 interface Todo {
   title: string;
   description: string;
   completed: boolean;
+  _id: string;
 }
 
-const newTodo = ref<{ title: string; description: string }>({ title: "", description: "" });
+const newTodo = ref<{ title: string; description: string }>({
+  title: "",
+  description: "",
+});
 const todos = ref<Todo[]>([]);
 const errorMessages = ref<string[]>([]);
 const config = useRuntimeConfig().public.api;
 
 async function addTodo() {
-  errorMessages.value = []; 
+  errorMessages.value = [];
 
   try {
     const data: { data: Todo } = await $fetch(`${config}/todos`, {
-      method: 'POST',
+      method: "POST",
       body: {
         title: newTodo.value.title,
         description: newTodo.value.description,
-      }
+      },
     });
 
     todos.value.unshift(data.data);
     newTodo.value = { title: "", description: "" };
-
   } catch (error: any) {
     if (error.data?.errorSources) {
-      errorMessages.value = error.data.errorSources.map((item: { path: string; message: string }) => item.message);
+      errorMessages.value = error.data.errorSources.map(
+        (item: { path: string; message: string }) => item.message
+      );
     } else {
-      errorMessages.value.push('An unknown error occurred.');
+      errorMessages.value.push("An unknown error occurred.");
     }
   }
 }
 
-function markAsComplete(index: number) {
-  todos.value[index].completed = !todos.value[index].completed; 
+async function markAsComplete(id: string, index: number) {
+  errorMessages.value = [];
+  console.log(todos)
+  try {
+    const data: { data: Todo } = await $fetch(`${config}/todos/${id}`, {
+      method: "PATCH",
+      body: {
+        completed: !todos.value[index].completed,
+      },
+    });
+    todos.value[index].completed = !todos.value[index].completed;
+  } catch (error: any) {
+    console.log(error.data);
+  }
 }
 
-function removeFromTodo(index: number) {
+async function removeFromTodo(id:string,index: number) {
+  errorMessages.value = []; // Clear any existing error messages
+
+try {
+  await $fetch(`${config}/todos/soft-delete/${id}`, {
+    method: "PATCH", 
+  });
+  
   todos.value.splice(index, 1);
+} catch (error: any) {
+  console.log(error.data);
+
+}
 }
 
 onMounted(async () => {
@@ -116,11 +153,14 @@ onMounted(async () => {
     todos.value = result.data.map((item: any) => ({
       title: item.title,
       description: item.description,
-      completed: item.completed || false, 
-    })); 
+      completed: item.completed || false,
+      _id: item._id,
+    }));
     console.log(result.data);
   } catch (error) {
-    console.error('Failed to fetch todos:', error);
+    console.error("Failed to fetch todos:", error);
   }
 });
-</script>
+</script> 
+
+
